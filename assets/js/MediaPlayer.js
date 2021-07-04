84e776e46b62aaacb2fea88f1dafa958a67adb36
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-07-04 11:58:12 -0500

    Clase 7 curso profesional de JS

---
+++ assets/js/MediaPlayer.js
@@ -0,0 +1,40 @@
+class MediaPlayer {
+  constructor(config) {
+    this.media = config.video;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+  }
+
+  _initPlugins() {
+    this.plugins.forEach((plugin) => {
+      plugin.run(this);
+    });
+  }
+
+  play() {
+    this.media.play();
+  }
+
+  pause() {
+    this.media.pause();
+  }
+
+  togglePlay() {
+    this.media.paused ? this.media.play() : this.media.pause();
+  }
+
+  mute() {
+    this.media.muted = true;
+  }
+
+  unmute() {
+    this.media.muted = false;
+  }
+
+  toggleMute() {
+    this.media.muted ? (this.media.muted = false) : (this.media.muted = true);
+  }
+}
+
+export default MediaPlayer;
---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
+const $video = document.querySelector('video');
+const $buttonPlay = document.querySelector('#buttonPlay');
+const $buttonMute = document.querySelector('#buttonMute');
+
+const player = new MediaPlayer({
+  video: $video,
+  plugins: [new AutoPlay()],
+});
+
+$buttonPlay.onclick = () => {
+  player.togglePlay();
+};
+
+$buttonMute.onclick = () => {
+  player.toggleMute();
+};
