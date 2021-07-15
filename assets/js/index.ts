a7e6f6914c0b02e267b9fb8c3a05791612860eee
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-07-14 20:01:53 -0500

    Logica de implementacion de Ads

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const $video: HTMLElement = document.querySelector('video');
 const $buttonPlay: HTMLElement = document.querySelector('#buttonPlay');
@@ -8,7 +9,7 @@ const $buttonMute: HTMLElement = document.querySelector('#buttonMute');
 
 const player = new MediaPlayer({
   video: $video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 $buttonPlay.onclick = () => {
--- ejercicios/singleton/index.ts
+++ ejercicios/singleton/index.ts
@@ -1,4 +1,4 @@
-import Singleton from './Singleton'
+import Singleton from './Singleton';
 
 const a = Singleton.getInstance();
 const b = Singleton.getInstance();
